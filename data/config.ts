8bab46d69dea8e9fe1f615f15e46ff8b1c8afe59
romadeoliveira3 <romadeoliveira3@gmail.com>
2025-10-12 15:47:38 -0300

    Refatorar a configuração dos ícones da área de trabalho para melhor legibilidade e manutenção

--- data/config.ts
+++ data/config.ts
@@ -1,14 +1,38 @@
-import { AboutIcon, ProjectsIcon, ContactIcon, GithubIcon, LinkedInIcon, ExperienceIcon, EducationIcon, LattesIcon } from '../components/icons/Icons';
-import type { DesktopIconType } from '../types';
-import { WindowType } from '../types';
+import {
+  AboutIcon,
+  ProjectsIcon,
+  ContactIcon,
+  GithubIcon,
+  LinkedInIcon,
+  ExperienceIcon,
+  EducationIcon,
+  LattesIcon,
+} from "../components/icons/Icons";
+import type { DesktopIconType } from "../types";
+import { WindowType } from "../types";
 
-export const DESKTOP_ICONS_CONFIG: Omit<DesktopIconType, 'label'>[] = [
-  { id: WindowType.ABOUT, icon: AboutIcon, type: 'window' },
-  { id: WindowType.PROJECTS, icon: ProjectsIcon, type: 'window' },
-  { id: WindowType.EXPERIENCE, icon: ExperienceIcon, type: 'window' },
-  { id: WindowType.EDUCATION, icon: EducationIcon, type: 'window' },
-  { id: 'GITHUB', icon: GithubIcon, type: 'link', url: 'https://github.com/romaveloso' },
-  { id: 'LINKEDIN', icon: LinkedInIcon, type: 'link', url: 'https://www.linkedin.com/' },
-  { id: 'LATTES', icon: LattesIcon, type: 'link', url: 'http://lattes.cnpq.br/9546596599129535' },
-  { id: WindowType.CONTACT, icon: ContactIcon, type: 'window' },
+export const DESKTOP_ICONS_CONFIG: Omit<DesktopIconType, "label">[] = [
+  { id: WindowType.ABOUT, icon: AboutIcon, type: "window" },
+  { id: WindowType.PROJECTS, icon: ProjectsIcon, type: "window" },
+  { id: WindowType.EXPERIENCE, icon: ExperienceIcon, type: "window" },
+  { id: WindowType.EDUCATION, icon: EducationIcon, type: "window" },
+  {
+    id: "GITHUB",
+    icon: GithubIcon,
+    type: "link",
+    url: "https://github.com/Romadeoliveira3",
+  },
+  {
+    id: "LINKEDIN",
+    icon: LinkedInIcon,
+    type: "link",
+    url: "https://www.linkedin.com/in/romario-jonas-veloso-427373175",
+  },
+  {
+    id: "LATTES",
+    icon: LattesIcon,
+    type: "link",
+    url: "http://lattes.cnpq.br/7040983804231610",
+  },
+  { id: WindowType.CONTACT, icon: ContactIcon, type: "window" },
 ];
